c2e0942ef48ab9ef065af65739d6a2be4272f06d
ajlachapelle <alachape@uoregon.edu>
2022-11-14 16:20:18 -0800

    autoupdate.sh revision

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,9 +1,13 @@
+git branch temp
+git checkout temp
 git rm --cached autoupdate.sh
+git commit -m 'Temp rebase commit'
+
 git checkout submodule
-git rebase dev
+git rebase temp
 git checkout --theirs *
 git add *
 git rm --cached autoupdate.sh
 git rebase --continue
-git checkout dev
-git restore --staged autoupdate.sh
+git checkout -f dev
+git branch -D temp
